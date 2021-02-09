eb5e661c295e69fec62e46d1d3f02188dcd3740a
Óscar Sánchez <oscarsanchezmeza97@gmail.com>
2021-02-09 03:45:17 +0000

    Design created and we are missing the functionality

---
+++ src/js/component/Game.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Game = () => {
+	return (
+		<div className="parent">
+			<div className="child">X</div>
+			<div className="child">X</div>
+			<div className="child">O</div>
+			<div className="child">O</div>
+			<div className="child">X</div>
+			<div className="child">O</div>
+			<div className="child">O</div>
+			<div className="child">X</div>
+			<div className="child">X</div>
+		</div>
+	);
+};
+
+export default Game;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import Form from "./Form.js";
+import Game from "./Game.js";
 //create your first component
 export function Home() {
+	let [firstName, setFirstName] = useState("");
+	let [secondName, setSecondName] = useState("");
+	let [next, setNext] = useState(false);
+
+	const changeState = () => {
+		setNext(true);
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1 className="main-title">
+				Tic Tac Toe with {firstName} {secondName}
+			</h1>
+			<div className="main">
+				<h2>Pick your weapon</h2>
+				<Form
+					setFirstName={setFirstName}
+					setSecondName={setSecondName}
+				/>
+				<div className="buttons">
+					<a href="#game" onClick={changeState} className="btnX">
+						X
+					</a>
+					<a href="#game" onClick={changeState} className="btnX">
+						O
+					</a>
+				</div>
+			</div>
+			{next ? <Game id="game" /> : null}
 		</div>
 	);
 }
